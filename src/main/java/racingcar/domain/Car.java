3095a8aaa4caf4180406e9bed66f65aa1ca33a03
donghyunkim <ghffu405@gmail.com>
2023-11-23 18:09:54 +0900

    feat: move 메소드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,28 @@
 package racingcar.domain;
 
 public class Car {
+
+    private static final int INIT_POSITION_NUMBER = 0;
+    private static final int MIN_NUMBER_FOR_MOVE = 4;
+    private String name;
+    private int position;
+
+    private Car(String name) {
+        this.name = name;
+        this.position = INIT_POSITION_NUMBER;
+    }
+
+    public static Car from(String name){
+        return new Car(name);
+    }
+
+    public void move(int randomNumber){
+        if(randomNumber >= MIN_NUMBER_FOR_MOVE){
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
 }
