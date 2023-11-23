7fd6325d1aa04c16672ff10ba77e2404dc30a7ef
donghyunkim <ghffu405@gmail.com>
2023-11-23 18:03:47 +0900

    feat: 차 생성 메소드 추가

--- src/main/java/racingcar/utils/CarsConverter.java
+++ src/main/java/racingcar/utils/CarsConverter.java
@@ -1,5 +1,7 @@
 package racingcar.utils;
 
+import racingcar.domain.Car;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,9 +10,15 @@ public class CarsConverter {
 
     private static final String SPLIT = ",";
 
-    public static List<String> parse(String names){
+    public static List<String> parse(String names) {
         String trimmedNames = names.trim();
         return Arrays.stream(trimmedNames.split(SPLIT))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Car> createCars(List<String> names){
+        return names.stream()
+                .map(name -> Car.from(name))
                 .collect(Collectors.toList());
     }
 }
