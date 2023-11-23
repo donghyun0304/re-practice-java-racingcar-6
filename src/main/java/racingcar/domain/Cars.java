1f59663ce332e78463602a56debd3a3a615467f6
donghyunkim <ghffu405@gmail.com>
2023-11-23 20:04:37 +0900

    feat: 우승자 찾는 메소드 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,36 @@
 package racingcar.domain;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Cars {
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<String> getWinners(){
+        List<Car> sortedCars = sortAscending(cars);
+        Car maxiestMovedCar = sortedCars.get(cars.size() - 1);
+        List<Car> winningCars = findCarsAtWinningPosition(sortedCars, maxiestMovedCar);
+        return new WinningCars(winningCars).getNames();
+    }
+
+    private List<Car> sortAscending(List<Car> cars){
+        return cars.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    private List<Car> findCarsAtWinningPosition(List<Car> cars, Car maxiestMovedCar){
+       return cars.stream()
+                .filter(car -> maxiestMovedCar.isSamePosition(car))
+                .collect(Collectors.toList());
+    }
+
+
 }
