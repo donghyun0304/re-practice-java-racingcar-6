fe210f23032293e3a5b8b168ef5b68a9afa018d1
donghyunkim <ghffu405@gmail.com>
2023-11-23 21:11:31 +0900

    feat: 차 이름 입력 검증 메소드 추가

--- src/main/java/racingcar/utils/validation/CarValidator.java
+++ src/main/java/racingcar/utils/validation/CarValidator.java
@@ -1,19 +1,48 @@
 package racingcar.utils.validation;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CarValidator {
 
     private static final int MIN_CAR_AMOUNT = 2;
+    private static final int MAX_CAR_NAME_LENGTH = 5;
 
-    public static final void validatorCarAmount(String input){
-        List<String> names = Arrays.asList(input.split(","));
-        if(names.size() < MIN_CAR_AMOUNT){
-            throw new IllegalArgumentException("[ERROR] 최소 2대의 차량이 존재해야 합니다.");
+    public static void validateNames(String input) {
+        if (isNullOrEmptyOrWhitespace(input)
+                || isSingleCar(input)
+                || isOverLength(input)
+                || hasDuplicateNames(input)
+        ) {
+            throw new IllegalArgumentException("[ERROR] 형식에 맞지 않습니다.");
         }
     }
 
+    private static boolean isNullOrEmptyOrWhitespace(String input) {
+        return input == null || input.trim().isEmpty();
+    }
 
+    private static boolean isSingleCar(String input) {
+        String[] names = input.split(",");
+        return names.length < MIN_CAR_AMOUNT;
+    }
+
+    private static boolean isOverLength(String input) {
+        return Arrays.stream(input.split(","))
+                .anyMatch(name -> name.length() > MAX_CAR_NAME_LENGTH);
+    }
+
+    private static boolean hasDuplicateNames(String input) {
+        List<String> names = Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
+        Set<String> nonDuplicateNames = new HashSet<>(names);
+        if (names.size() != nonDuplicateNames.size()) {
+            return true;
+        }
+        return false;
+    }
 
 }
