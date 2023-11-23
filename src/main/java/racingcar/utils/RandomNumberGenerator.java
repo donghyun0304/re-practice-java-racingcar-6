39e20ef0f77fea4e99fc568d5aa01aeb9776e84c
donghyunkim <ghffu405@gmail.com>
2023-11-23 17:27:12 +0900

    feat: 랜던넘버 메소드 추ㅏㄱ

--- src/main/java/racingcar/utils/RandomNumberGenerator.java
+++ src/main/java/racingcar/utils/RandomNumberGenerator.java
@@ -1,4 +1,24 @@
 package racingcar.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RandomNumberGenerator {
+
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+
+
+    public static int createNumber(){
+        int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+        validateNumberRange(randomNumber);
+        return randomNumber;
+    }
+
+    private static void validateNumberRange(int randomNumber){
+        if(!(randomNumber >= MIN_NUMBER && randomNumber <= MAX_NUMBER)){
+            throw new IllegalArgumentException("[ERROR] 0~9 사이의 숫자가 아닙니다.");
+        }
+    }
+
+
 }
