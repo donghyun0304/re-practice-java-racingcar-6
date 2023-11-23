6d418d904901202f06ffc44f52e384e93491767f
donghyunkim <ghffu405@gmail.com>
2023-11-23 20:16:08 +0900

    feat: getter 추가

--- src/main/java/racingcar/domain/Result.java
+++ src/main/java/racingcar/domain/Result.java
@@ -1,4 +1,17 @@
 package racingcar.domain;
 
+import java.util.Collections;
+import java.util.Map;
+
 public class Result {
+
+    private final Map<String, Integer> result;
+
+    public Result(Map<String, Integer> result) {
+        this.result = result;
+    }
+
+    public Map<String, Integer> getResult() {
+        return Collections.unmodifiableMap(result);
+    }
 }
