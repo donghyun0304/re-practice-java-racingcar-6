2c408181470b6f38444a362a669aadd7e24236ce
donghyunkim <ghffu405@gmail.com>
2023-11-23 18:15:52 +0900

    feat: 서비스 로직 추가

--- src/main/java/racingcar/service/RacingCarService.java
+++ src/main/java/racingcar/service/RacingCarService.java
@@ -1,4 +1,15 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.utils.RandomNumberGenerator;
+
+import java.util.List;
+
 public class RacingCarService {
+
+    public void moveCars(List<Car> cars){
+        for(Car car : cars){
+            car.move(RandomNumberGenerator.createNumber());
+        }
+    }
 }
